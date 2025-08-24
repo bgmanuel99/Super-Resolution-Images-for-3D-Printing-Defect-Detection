0d8d7d2d27ae4cf2665e3781c0f191ad38b7bb4e
bgmanuel99 <bgmanuel1999@gmail.com>
2025-08-24 20:04:18 +0200

    advanced data augmentation for all deep learning models

--- SRModels/constants.py
+++ SRModels/constants.py
@@ -6,5 +6,3 @@ STRIDE = 12
 INTERPOLATION = cv2.INTER_CUBIC
 SCALE_FACTOR = 2
 RANDOM_SEED = 42
-HR_ROOT = os.path.abspath(os.path.join(os.getcwd(), "../data/images/HR"))
-LR_ROOT = os.path.abspath(os.path.join(os.getcwd(), "../data/images/LR"))
